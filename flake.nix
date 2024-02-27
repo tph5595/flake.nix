c814c3ff06ed808cba22ad649efeb441a7e02baf
Taylor Henderson <tph5595@verizon.net>
2024-02-27 15:48:23 -0500

    updating shell

--- flake.nix
+++ flake.nix
@@ -8,25 +8,25 @@
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # Controls system level software and settings including fonts
+# Controls system level software and settings including fonts
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = inputs@{ nixpkgs, home-manager, darwin, ... }: {
-    darwinConfigurations.BestBox = darwin.lib.darwinSystem {
-      system = "x86_64-darwin";
-      pkgs = import nixpkgs { system = "x86_64-darwin"; };
-      modules = [
-        ./modules/darwin
-        home-manager.darwinModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.taylor.imports = [ ./modules/home-manager ];
-          };
-        }
-      ];
-    };
+      darwinConfigurations.BestBox = darwin.lib.darwinSystem {
+          system = "x86_64-darwin";
+          pkgs = import nixpkgs { system = "x86_64-darwin"; };
+          modules = [
+              ./modules/darwin
+                  home-manager.darwinModules.home-manager
+                  {
+                      home-manager = {
+                          useGlobalPkgs = true;
+                          useUserPackages = true;
+                          users.taylor.imports = [ ./modules/home-manager ];
+                      };
+                  }
+          ];
+      };
   };
 }
--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,28 +1,61 @@
 { pkgs, ... }: {
-  # Don't change this when you change package input. Leave it alone.
-  home.stateVersion = "23.11";
-  # specify my home-manager configs
-  home.packages = with pkgs; [
-    ripgrep
-    fd
-    curl
-    less
-    neovim
-  ];
-  home.sessionVariables = {
-    CLICLOLOR = 1;
-    EDITOR = "nvim";
-  };
-  programs.fzf.enable = true;
-  programs.fzf.enableZshIntegration = true;
-  programs.git.enable = true;
-  programs.zsh.enable = true;
-  programs.zsh.enableCompletion = true;
-  programs.zsh.enableAutosuggestions = true;
-  programs.zsh.syntaxHighlighting.enable = true;
-  programs.zsh.shellAliases = {
-    nixswitch = "darwin-rebuild switch --flake ~/src/system-config/.#";
-    nixup = "pushd ~/src/system-config; nix flake update; nixswitch; popd";
-  };
-  # home.file.".inputrc".source = ./dotfiles/inputrc;
+# Don't change this when you change package input. Leave it alone.
+    home.stateVersion = "23.11";
+# specify my home-manager configs
+    home.packages = with pkgs; [
+        ripgrep
+        fd
+        curl
+        less
+        neovim
+        zsh-vi-mode
+        zsh-powerlevel10k
+        kitty
+    ];
+    home.sessionVariables = {
+        CLICLOLOR = 1;
+        VISUAL="nvim";
+        EDITOR="$VISUAL";
+        BROWSER="firefox";
+    };
+
+    programs.fzf.enable = true;
+    programs.fzf.enableZshIntegration = true;
+
+    programs.git.enable = true;
+
+    programs.zsh = {
+        enable = true;
+        enableCompletion = true;
+        enableAutosuggestions = true;
+        syntaxHighlighting.enable = true;
+#        oh-my-zsh = {
+#            enable = true;
+#            plugins = [ "git" "k" "zsh-vi-mode" ];
+#            theme = "robbyrussel";
+#        };
+        zplug = {
+            enable = true;
+            plugins = [
+            { name = "romkatv/powerlevel10k"; tags = ["as:theme" "depth:1"]; }
+            { name = "plugins/git"; tags = [from:oh-my-zsh]; }
+            { name = "plugins/zsh-vi-mode"; tags = [from:oh-my-zsh]; }
+            ];
+        };
+        shellAliases = {
+            nixswitch = "darwin-rebuild switch --flake ~/Desktop/flake.nix/.#";
+            nixup = "pushd ~/src/system-config; nix flake update; nixswitch; popd";
+            l = "ls -lah";
+            c = "clear";
+            cl = "c && ls";
+            ga = "git add";
+            gc = "git commit";
+            gs = "git status";
+            gps = "git push";
+            gpl = "git pull";
+            revealjs = "pandoc -t revealjs -s --mathjax -i --variable transition=none --variable controls=false --variable controlsTutorial=false --variable slideNumber=true --variable fragments=false --variable theme=dracula -o ";
+            ssh = "kitty +kitten ssh";
+        };
+    };
+    home.file.".p10k.zsh".source = ./dotfiles/p10k.zsh;
 }
