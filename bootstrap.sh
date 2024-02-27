5d1fcda0411b9a5f7af7abfb451789ffd220ba1a
Taylor Henderson <tph5595@verizon.net>
2024-02-27 12:32:19 -0500

    ignore result but activate it

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # sh <(curl -L https://nixos.org/nix/install) --daemon
 nix build --extra-experimental-features "nix-command flakes" .#darwinConfigurations.BestBox.system
+./result/sw/bin/darwin-rebuild switch --flake .#BestBox
