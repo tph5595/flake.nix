95c1891be12773e94ed71f0c52e77228c86655f0
Taylor Henderson <tph5595@verizon.net>
2024-03-06 09:13:32 -0500

    adding support

--- flake.nix
+++ flake.nix
@@ -12,7 +12,19 @@
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs@{ nixpkgs, home-manager, darwin, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, darwin, ... }: 
+  let 
+      # Systems supported
+      allSystems = [
+          "x86_64-linux" # 64-bit Intel/AMD Linux
+          "x86_64-darwin" # 64-bit Intel macOS
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+          pkgs = import nixpkgs { inherit system; };
+          });
+  in 
+  {
       darwinConfigurations.BestBox = darwin.lib.darwinSystem {
           system = "x86_64-darwin";
           pkgs = import nixpkgs { system = "x86_64-darwin"; };
