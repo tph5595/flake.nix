0475678f22b9a88f7e9cf7e1c8c589c21d26eed2
Taylor Henderson <tph5595@verizon.net>
2024-03-12 12:34:58 -0400

    fugative bugs

--- modules/home-manager/dotfiles/nvim/lua/taylorhenderson/plugins/fugitive.lua
+++ modules/home-manager/dotfiles/nvim/lua/taylorhenderson/plugins/fugitive.lua
@@ -1,7 +1,13 @@
 return {
-    "tpope/vim-fugitive",
-    conifg = function ()
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-        vim.keymap.set("n", "<leader>gp", function () vim.cmd("Git push")end )
-    end
+    {
+        "tpope/vim-fugitive",
+        lazy = true,
+        keys = {
+            { "<leader>gs", "", desc = "fugitive toggle" },
+        },
+        conifg = function ()
+            vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+            vim.keymap.set("n", "<leader>gp", function () vim.cmd("Git push")end )
+        end,
+    }
 }
