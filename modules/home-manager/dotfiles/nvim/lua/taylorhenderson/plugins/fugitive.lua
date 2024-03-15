9c5f319fa148587190d844ca0badb383504204e5
Taylor Henderson <tph5595@verizon.net>
2024-03-15 08:47:59 -0400

    working git fugitive

--- modules/home-manager/dotfiles/nvim/lua/taylorhenderson/plugins/fugitive.lua
+++ modules/home-manager/dotfiles/nvim/lua/taylorhenderson/plugins/fugitive.lua
@@ -1,10 +1,10 @@
 return {
     {
         "tpope/vim-fugitive",
-        lazy = true,
-        keys = {
-            { "<leader>gg", "", desc = "fugitive toggle" },
-        },
+        -- lazy = true,
+        -- keys = {
+        --     { "<leader>gg", "", desc = "fugitive toggle" },
+        -- },
         config = function ()
             vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
             vim.keymap.set("n", "<leader>gp", function () vim.cmd("Git push")end )
