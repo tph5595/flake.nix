67cbd69bcb23d06c9d670e830202297d452a4925
Taylor Henderson <tph5595@verizon.net>
2024-03-31 15:23:56 -0400

    move to nix

--- modules/home-manager/pop.nix
+++ modules/home-manager/pop.nix
@@ -4,6 +4,8 @@
 
     programs.home-manager.enable = true;
 
+    nixpkgs.config.allowUnfree = true;
+
     home.packages = with pkgs; [
             kitty
             ffmpeg_5-full
@@ -11,6 +13,17 @@
             openvpn
             subversion
             darktable
+            bitwig-studio
+            discord
+            htop
+            librewolf
+            steam
+            zoom-us
+            davinci-resolve
+            anki
+            vlc
+            tidal-hifi
+            golden-cheetah
             ];
     home.sessionVariables = {
         BROWSER = "firefox";
