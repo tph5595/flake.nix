317c95dd5fa16763f3effb6068ee1f282d06b9d4
Taylor Henderson <tph5595@verizon.net>
2024-04-23 17:21:51 -0400

    minor clean up

--- flake.nix
+++ flake.nix
@@ -29,18 +29,7 @@
           modules = [
               ./modules/darwin
               agenix.darwinModules.age
-              {
-                  age = {
-                    secrets."care_ssh" = {
-                        file = ./secrets/care_ssh.age;
-                        path = "/Users/taylor/test_secret";
-                        owner = "taylor";
-                    };
-                    secretsDir = "/Users/taylor/.agenix/agenix";
-                    secretsMountPoint = "/Users/taylor/.agenix/agenix.d";
-                    identityPaths = [ "/Users/taylor/.ssh/id_ed25519" ];
-                  };
-              }
+              ./secrets/darwin.nix
               home-manager.darwinModules.home-manager
               {
                   home-manager = {
---
+++ secrets/darwin.nix
@@ -0,0 +1,12 @@
+{
+    age = {
+        secrets."care_ssh" = {
+            file = ./care_ssh.age;
+            path = "/Users/taylor/test_secret";
+            owner = "taylor";
+        };
+        secretsDir = "/Users/taylor/.agenix/agenix";
+        secretsMountPoint = "/Users/taylor/.agenix/agenix.d";
+        identityPaths = [ "/Users/taylor/.ssh/id_ed25519" ];
+    };
+}
