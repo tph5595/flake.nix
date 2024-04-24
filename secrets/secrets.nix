86a2caa311cdcf564f0f1403667e6a40e8d77a9f
Taylor Henderson <tph5595@verizon.net>
2024-04-24 11:08:25 -0400

    add key for sr3s13

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   BestBox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDgl9C/J+Qtaab4mkGpuhIlAHS1d7XUTL64KLSbhCZ7w taylorhenderson@pm.me";
-  sr3s13 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL0idNvgGiucWgup/mP78zyC23uFjYq0evcWdjGQUaBH";
+  sr3s13 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC9M83BK8IEW3r6e16n4iO8M8lwy3mg/79AaByytdeL7 thender8@sr3s13";
   taylor = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGJhGM7OLxBYjZR0HdKZadqSemsPuyxdMc5UYRIb+fbj taylor@pop-os";
   personal = [ BestBox taylor ];
   gmucare = [ sr3s13 ];
