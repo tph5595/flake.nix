ee8700cf7eea191569f0c4c64a5d49b8ba51f10c
Taylor Henderson <tph5595@verizon.net>
2024-04-26 15:51:23 -0400

    add kotlin

--- templates/default.nix
+++ templates/default.nix
@@ -1,19 +1,23 @@
-      {
-          python = {
-              path = ./dev/python;
-              description = "Python dev environment template";
-          };
+{
+    python = {
+        path = ./dev/python;
+        description = "Python dev environment template";
+    };
 
-          rust = {
-              path = ./dev/rust;
-              description = "Rust dev environment template";
-          };
-          bash = {
-              path = ./default;
-              description = "nix flake new -t github:Mic92/nix-direnv .";
-          };
-          latex = {
-              path = ./dev/latex;
-              description = "Latex dev environment template";
-          };
-      }
+    rust = {
+        path = ./dev/rust;
+        description = "Rust dev environment template";
+    };
+    bash = {
+        path = ./default;
+        description = "nix flake new -t github:Mic92/nix-direnv .";
+    };
+    latex = {
+        path = ./dev/latex;
+        description = "Latex dev environment template";
+    };
+    kotlin = {
+        path = ./dev/kotlin;
+        description = "kotlin dev environment template";
+    };
+}
