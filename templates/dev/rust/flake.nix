7a9f2716cfc37ec0162a516b3dc1d17012067153
Taylor Henderson <tph5595@verizon.net>
2024-04-26 16:13:35 -0400

    better rust env

--- templates/dev/rust/flake.nix
+++ templates/dev/rust/flake.nix
@@ -2,8 +2,7 @@
   description = "Rusty";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs"; # also valid: "nixpkgs"
-    # nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,18 +14,7 @@
       overlays = [
         rust-overlay.overlays.default
         (final: prev: {
-          rustToolchain =
-            let
-              rust = prev.rust-bin;
-            in
-            if builtins.pathExists ./rust-toolchain.toml then
-              rust.fromRustupToolchainFile ./rust-toolchain.toml
-            else if builtins.pathExists ./rust-toolchain then
-              rust.fromRustupToolchainFile ./rust-toolchain
-            else
-              rust.stable.latest.default.override {
-                extensions = [ "rust-src" "rustfmt" ];
-              };
+          rustToolchain = prev.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
         })
       ];
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
