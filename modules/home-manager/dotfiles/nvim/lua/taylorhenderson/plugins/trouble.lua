e65a6e076fd0469cc44641b8b0798cff7e98fffb
Taylor Henderson <tph5595@verizon.net>
2024-05-01 11:37:53 -0400

    trouble fix

--- modules/home-manager/dotfiles/nvim/lua/taylorhenderson/plugins/trouble.lua
+++ modules/home-manager/dotfiles/nvim/lua/taylorhenderson/plugins/trouble.lua
@@ -1,13 +1,14 @@
 return {
     {
         "https://github.com/folke/trouble.nvim",
-        lazy = true,
-        keys = {
-            { "<leader>tt", "<cmd> require(\"trouble\").toggle() <cr>", desc = "Copilot toggle" },
-        },
+        -- lazy = true,
+        -- keys = {
+        --     { "<leader>tt", "<cmd> require(\"trouble\").toggle() <cr>", desc = "Copilot toggle" },
+        -- },
         config = function ()
-            -- vim.keymap.set("n", "<leader>tt", function()
-            -- end)
+            vim.keymap.set("n", "<leader>tt", function()
+                require("trouble").toggle()
+            end)
             -- jump to the next item, skipping the groups
             vim.keymap.set("n", "<leader>[d", function()
                 require("trouble").next({skip_groups = true, jump = true})
