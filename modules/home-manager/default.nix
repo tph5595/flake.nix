95abd24f5ff5a0d3b24f2acb11ec7465d166fc79
Taylor Henderson <tph5595@verizon.net>
2024-05-14 07:44:28 -0400

    setup my own vi mode

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -55,7 +55,12 @@
         syntaxHighlighting.enable = true;
         autocd = false;
         initExtraFirst = ''
+            bindkey -v
             bindkey -s ^f "~/.local/bin/tmux-sessionizer\n"
+            bindkey "^H" backward-delete-char
+            bindkey "^?" backward-delete-char
+            bindkey '^P' history-incremental-search-backward
+            bindkey '^N' history-incremental-search-forward
 
             # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
             # Initialization code that may require console input (password prompts, [y/n]
