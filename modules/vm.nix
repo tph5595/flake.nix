7ab51acfa07c049aa8755b50eb5787fd36b8826a
Taylor Henderson <tph5595@verizon.net>
2024-09-06 11:41:07 -0400

    working windows vm. no dGPU

--- modules/vm.nix
+++ modules/vm.nix
@@ -4,15 +4,29 @@
     options = {
         vm.enable = 
             lib.mkEnableOption "enables vms";
+        vm.libvirtd_user = 
+            lib.mkOption {
+                type = with lib.types; str;
+                default = "taylor";
+                description = "Set libvirtd user";
+            };
     };
     config = lib.mkIf config.vm.enable {
         environment.systemPackages = with pkgs; [
             virt-manager
+            libguestfs
         ];
+
+        users.users.${config.vm.libvirtd_user}= {
+            extraGroups = [ "libvirtd" ];
+        };
+
         virtualisation = {
             libvirtd = {
                 enable = true;
                 qemu = {
+                    package = pkgs.qemu_kvm;
+                    runAsRoot = true;
                     swtpm.enable = true;
                     ovmf.enable = true;
                     ovmf.packages = [ pkgs.OVMFFull.fd ];
