3d2bacec44409c9c1da18cc2b6388e572f7a954f
Taylor Henderson <tph5595@verizon.net>
2024-09-16 17:58:26 +0000

    better kitty setup

--- home-manager/modules/kitty.nix
+++ home-manager/modules/kitty.nix
@@ -1,10 +1,65 @@
-{ pkgs, ... }: 
+{ config, pkgs, lib, ... }: 
+let nixGL = import ./nixGL.nix { inherit pkgs config; };
+in
 {
-    #home.packages = with pkgs; [
-        #kitty
-    #];
-    home.file."./.config/kitty/" = {
-        source = ../../config/kitty;
-        recursive = true;
+    options.nixGLPrefix = lib.mkOption {
+        type = lib.types.str;
+        default = "";
+        description = ''
+            Will be prepended to commands which require working OpenGL.
+
+            This needs to be set to the right nixGL package on non-NixOS systems.
+            '';
     };
-}
+    options = {
+        kitty.enable = 
+            lib.mkEnableOption "enables kitty";
+        kitty.nixGL = 
+            lib.mkEnableOption "enables nixGL for kitty";
+        kitty.hide_borders = 
+            lib.mkOption {
+                type = with lib.types; str;
+                default = "yes";
+                description = "Whether to show window borders";
+            };
+        kitty.font_size = 
+            lib.mkOption {
+                type = with lib.types; str;
+                default = "18.0";
+                description = "Font size for kitty";
+            };
+    };
+    config = lib.mkIf config.kitty.enable {
+        home.packages = with pkgs; 
+        [ ] ++ (if config.kitty.nixGL then
+            [ (nixGL kitty)]
+          else [
+            kitty
+          ]);
+        home.file."./.config/kitty/rose-pine.conf" = {
+            source = ../../config/kitty/rose-pine.conf;
+        };
+        home.file."./.config/kitty/kitty.conf" = {
+            text = ''
+                include rose-pine.conf
+
+                #Font-Family
+                font_family Hack Regular Nerd Font Complete Mono
+                italic_font auto
+                bold_font auto
+                bold_italic_font auto
+
+                hide_window_decorations ${config.kitty.hide_borders}
+
+                #Font size (in pts)
+                font_size ${config.kitty.font_size}
+
+                #Foreground color
+                # foreground #ffffff
+
+                #Background color
+                # background #7d2ac9
+            '';
+        };
+        };
+    }
--- home-manager/vecServer.nix
+++ home-manager/vecServer.nix
@@ -1,35 +1,26 @@
 { pkgs, lib, config, ... }: 
-
-let nixGL = import ./modules/nixGL.nix { inherit pkgs config; };
-in
 {
-    options.nixGLPrefix = lib.mkOption {
-        type = lib.types.str;
-        default = "";
-        description = ''
-            Will be prepended to commands which require working OpenGL.
-
-            This needs to be set to the right nixGL package on non-NixOS systems.
-            '';
+    imports = [
+        ./modules/kitty.nix
+    ];
+    kitty = {
+        enable = true;
+        font_size = "12.0";
+        hide_borders = "no";
+        nixGL = true;
     };
-        imports = [
-            ./modules/kitty.nix
-        ];
-    config = {
-        home.username = "taylor";
-        home.homeDirectory = "/home/taylor";
 
-        programs.home-manager.enable = true;
+    home.username = "taylor";
+    home.homeDirectory = "/home/taylor";
 
-        home.packages = with pkgs; [
-            pinentry
-            (nixGL kitty)
-        ];
+    programs.home-manager.enable = true;
 
-        programs.zsh.shellAliases.nixswitch = "home-manager switch --flake ~/flake.nix/.#$HOST";
-        programs.zsh.initExtra = ''
-            . /home/$USER/.nix-profile/etc/profile.d/nix.sh
-            '';
+    home.packages = with pkgs; [
+        pinentry
+    ];
 
-    };
+    programs.zsh.shellAliases.nixswitch = "home-manager switch --flake ~/flake.nix/.#$HOST";
+    programs.zsh.initExtra = ''
+        . /home/$USER/.nix-profile/etc/profile.d/nix.sh
+        '';
 }
