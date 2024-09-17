c23054921687223451a7bc4c4904dd4e1887dbbb
Taylor Henderson <taylor.henderson@jhuapl.edu>
2024-09-17 09:47:30 -0400

    fix kitty

--- home-manager/slimDev.nix
+++ home-manager/slimDev.nix
@@ -1,13 +1,12 @@
 { pkgs, ... }: {
     imports = [
-        ./kitty.nix
+        ./modules/kitty.nix
     ];
     home.packages = with pkgs; [
         pinentry_mac
     ];
     home.sessionVariables = {
         BROWSER = "firefox";
-        # NIX_SSL_CERT_FILE = "/Users/hendetp1/root-cert.cer";
     };
 
     programs.zsh.shellAliases.nixswitch = "darwin-rebuild switch --flake ~/flake.nix/.#slimDev";
