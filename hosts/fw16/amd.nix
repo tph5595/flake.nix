30d5978177b060c21101847d7938f85e78a3794f
Taylor Henderson <tph5595@verizon.net>
2024-12-18 17:12:12 -0500

    option renamed

--- hosts/fw16/amd.nix
+++ hosts/fw16/amd.nix
@@ -1,20 +1,20 @@
 { config, lib, pkgs, ... }:
 {
 
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    driSupport32Bit = true;
-      extraPackages = with pkgs; [
+    enable32Bit = true;
+    extraPackages = with pkgs; [
         amdvlk
-        libva
-        libvdpau
-        libvdpau-va-gl
-        vaapiVdpau
-        rocmPackages.clr.icd
-      ];
-      extraPackages32 = [
+            libva
+            libvdpau
+            libvdpau-va-gl
+            vaapiVdpau
+            rocmPackages.clr.icd
+    ];
+    extraPackages32 = [
         pkgs.driversi686Linux.amdvlk
-      ];
+    ];
   };
 
   services.xserver.videoDrivers = ["amdgpu"];
