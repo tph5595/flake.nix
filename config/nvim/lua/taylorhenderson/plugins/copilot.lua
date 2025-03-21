56e7f291eac8dbc8c6778a825389511da13e172e
Taylor Henderson <tph5595@verizon.net>
2025-03-21 11:59:19 -0400

    disable copilot

--- config/nvim/lua/taylorhenderson/plugins/copilot.lua
+++ config/nvim/lua/taylorhenderson/plugins/copilot.lua
@@ -1,21 +1,21 @@
-local copilot_on = false
-vim.api.nvim_create_user_command("CopilotToggle", function()
-	if copilot_on then
-		vim.cmd("Copilot disable")
-		print("Copilot OFF")
-	else
-		vim.cmd("Copilot enable")
-		print("Copilot ON")
-	end
-	copilot_on = not copilot_on
-end, { nargs = 0 })
+-- local copilot_on = false
+-- vim.api.nvim_create_user_command("CopilotToggle", function()
+-- 	if copilot_on then
+-- 		vim.cmd("Copilot disable")
+-- 		print("Copilot OFF")
+-- 	else
+-- 		vim.cmd("Copilot enable")
+-- 		print("Copilot ON")
+-- 	end
+-- 	copilot_on = not copilot_on
+-- end, { nargs = 0 })
 
-return {
-    {
-        "github/copilot.vim",
-        lazy = true,
-        keys = {
-            { "<leader>c", "<cmd>CopilotToggle<cr>", desc = "Copilot toggle" },
-        },
-    }
-}
+-- return {
+--     {
+--         "github/copilot.vim",
+--         lazy = true,
+--         keys = {
+--             { "<leader>c", "<cmd>CopilotToggle<cr>", desc = "Copilot toggle" },
+--         },
+--     }
+-- }
