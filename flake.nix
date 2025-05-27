a82c32a0266e31c6933b27b983ea5747a6e77dd2
Taylor Henderson <tph5595@verizon.net>
2025-05-27 12:50:56 -0400

    upgrade to 25.05

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
   description = "Taylor's Nix configuration";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     # Manages configs links things into your home directory
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Controls MacOS system level software and settings including fonts
--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -10,39 +10,41 @@
         autosuggestion.enable = false;
         syntaxHighlighting.enable = true;
         autocd = false;
-        initExtraFirst = ''
-            bindkey -v
-            bindkey -s ^f "~/.local/bin/tmux-sessionizer\n"
-            bindkey "^H" backward-delete-char
-            bindkey "^?" backward-delete-char
-            bindkey '^r' history-incremental-search-backward
-            bindkey '^p' up-line-or-search
-            bindkey '^n' down-line-or-search
-            export PATH=~/.local/bin/:$PATH
+        initContent = 
+            let topofzshrc = lib.mkBefore ''
+                bindkey -v
+                bindkey -s ^f "~/.local/bin/tmux-sessionizer\n"
+                bindkey "^H" backward-delete-char
+                bindkey "^?" backward-delete-char
+                bindkey '^r' history-incremental-search-backward
+                bindkey '^p' up-line-or-search
+                bindkey '^n' down-line-or-search
+                export PATH=~/.local/bin/:$PATH
 
-            # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-            # Initialization code that may require console input (password prompts, [y/n]
-            # confirmations, etc.) must go above this block; everything else may go below.
-            if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-                source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-            fi
-        '';
-        initExtra = ''
-            # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-            [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+                # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+                # Initialization code that may require console input (password prompts, [y/n]
+                # confirmations, etc.) must go above this block; everything else may go below.
+                if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+                    source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+                fi
+            ''; 
+            generalzshrc = ''
+                # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+                [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
-            zstyle ':completion:*' menu select
-            zstyle ':completion:*:default' list-colors ''${(s.:.)LS_COLORS} "ma=48;5;244;1"
-            zstyle ':completion:*' matcher-list ''\'' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+                zstyle ':completion:*' menu select
+                zstyle ':completion:*:default' list-colors ''${(s.:.)LS_COLORS} "ma=48;5;244;1"
+                zstyle ':completion:*' matcher-list ''\'' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
-            # Ensure agenix is running to keep secrets up to date
-            # if [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
-                # systemctl --user start agenix.service
-            # fi
-            cd $HOME
+                # Ensure agenix is running to keep secrets up to date
+                # if [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
+                    # systemctl --user start agenix.service
+                # fi
+                cd $HOME
 
-            export LD_LIBRARY_PATH=${pkgs.libGL}/lib/
-            '';
+                export LD_LIBRARY_PATH=${pkgs.libGL}/lib/
+                ''; 
+            in lib.mkMerge [ topofzshrc generalzshrc];
         envExtra = ''
             EDITOR=nvim
         '';
